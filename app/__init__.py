bf0e6f4194941901ff7248034f5df0f3d02e0c8a
KarLOL82 <klinfeldt@gmail.com>
2022-11-24 18:14:49 -0800

    finalizing homepage views

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes import home, dashboard
+from app.db import init_db
 
 
 def create_app(test_config=None):
@@ -9,7 +10,7 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
-
+  
   @app.route('/hello')
   def hello():
     return 'hello world'
@@ -18,4 +19,6 @@ def create_app(test_config=None):
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
 
+  
+  init_db()
   return app
